810d438bb7a5c90df0413e4f2833c24680586f1a
AubreyVel <agvelasco@amarillocollege.com>
2025-04-23 16:51:58 -0500

    Add project files.

---
+++ Wk4Ex3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk4Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a word to manipulate or test.");
+            string userString = Console.ReadLine();
+
+            Console.WriteLine(ReverseString(userString));
+            
+        }
+
+        public static string ReverseString(string userString)
+        {
+            string newString = "";
+
+
+
+            Console.WriteLine(newString);
+            
+        }
+       public static string CountVowels(string userString)
+        {
+
+        }
+       public static string IsPalindrome(string userString)
+        {
+
+            string newString = "";
+
+
+            if (userString == newString)
+            {
+                Console.WriteLine("The word: "+userString+ "IS a palindrome.");
+            }
+
+            else
+            {
+                Console.WriteLine("The word:"+userString+"is NOT a Palindrome");
+            }
+        }
+    }
+}
